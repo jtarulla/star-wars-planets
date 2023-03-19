4fe954d66d86c96e66f6ec5f2031f02bcae2c672
Javier <jtarulla@gmail.com>
2023-03-19 18:22:28 +0100

    Routes: router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import './App.css'
+import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import router from '@/application/routes/routes'
 
 function App() {
-  return <div className="App">hello, world!</div>
+  return (
+    <div className="App">
+      <ToastContainer />
+      <RouterProvider router={router} />
+    </div>
+  )
 }
 
 export default App
